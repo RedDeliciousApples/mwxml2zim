f8fe10848d73afcf3cb2607cd2009eea18757d7d
RedDeliciouusApples <red-delicious-apple-hater@protonmail.com>
2023-08-17 20:43:16 -0700

    forget it, just remove the templates...

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from wikitextprocessor import Wtp, WikiNode, NodeKind, Page
 
 import filewriter
+import re
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -15,19 +16,19 @@ def page_handler(page: Page) -> [1, 2, 3]:
         return ["fail on page " + page.title]
     #    tree = parser.parse(page.text, pre_expand=True)
     print("breakpoint page processed: " + page.title)
-    node = parser.parse(str(page.body), True, True)
-    #TODO big improvements needed for node_to_html, maybe use mwparserfromhell?
-    filewriter.write("my_file-{}.html".format(page.title) , parser.node_to_html(node))
-    print("page text: " + str(node))
+    text = removeTemplate(page.body)
+    # TODO big improvements needed for node_to_html, maybe use mwparserfromhell?
+    filewriter.write("my_file-{}.html".format(page.title), text)
+    print("page text: " + text)
 
 
 def process_dump(path):
     namespaces = {0, 10}
     search_tree = list(parser.process(path, page_handler, namespaces))
     # we need all pages in Module: namespace, figuring out how to get these...
-    #for e in search_tree:
-       # print("iterated: " + str(len(e)))
-        #print("\n\nhtml:\n\n" + parser.node_to_html(e))
+    # for e in search_tree:
+    # print("iterated: " + str(len(e)))
+    # print("\n\nhtml:\n\n" + parser.node_to_html(e))
 
     print("dump finish")
 
@@ -35,18 +36,30 @@ def process_dump(path):
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    modulePath = "Wikipedia-popular-modules.xml"
+    # modulePath = "Wikipedia-popular-modules.xml"
     filePath = "appletrainboxtemplates.xml"
-    load_modules(modulePath)
+    # load_modules(modulePath)
     process_dump(filePath)
+
 
 def load_modules(path):
     namespaces = {828}
     print("Beegin module load...\n")
     list(parser.process(path, page_handler, namespaces))
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+def removeTemplate(input_string: str) -> str:
+    # Define a regular expression pattern to match text between curly braces
+    pattern = r'\{{.*?\}}'
+
+    # Use re.sub() to replace the matched pattern with an empty string
+    result = re.sub(pattern, '', input_string)
+
+    return result
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
