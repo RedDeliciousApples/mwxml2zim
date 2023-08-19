9ad9fba43d2b7607ec03e0c77cd6bb8fbe5de173
RedDeliciouusApples <red-delicious-apple-hater@protonmail.com>
2023-08-19 13:27:30 -0700

    some more heading levels, fix filewriter.write()

--- filewriter.py
+++ filewriter.py
@@ -1,10 +1,5 @@
-def write(file_path, content):
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-        print("File written successfully.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def write(content, list):
+    list.add(content)
 def init() -> list:
     #starts a list with open html template
     htmlList = []
--- html.py
+++ html.py
@@ -1,10 +1,28 @@
 import filewriter
 
+list = filewriter.init()
+
 
 def level2(text):
     content = "<h2> " + text + "</h2> <br />"
-    filewriter.write("my_page.html", content)
+    filewriter.write(content, list)
+
 
 def level3(text):
     content = "<h3> " + text + "</h3> <br />"
-    filewriter.write("my_page.html", content)
+    filewriter.write(content, list)
+
+
+def level4(text):
+    content = "<h4> " + text + "</h4> <br />"
+    filewriter.write(content, list)
+
+
+def level5(text):
+    content = "<h5> " + text + "</h5> <br />"
+    filewriter.write(content, list)
+
+
+def level6(text):
+    content = "<h6> " + text + "</h6> <br />"
+    filewriter.write(content, list)
