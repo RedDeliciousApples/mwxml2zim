120c810f1862393ff1375b7569e2804c5619184a
RedDeliciouusApples <red-delicious-apple-hater@protonmail.com>
2023-08-19 13:23:18 -0700

    add writeClose

--- filewriter.py
+++ filewriter.py
@@ -16,3 +16,15 @@ def init() -> list:
         </head>
         <body>""")
     return htmlList
+
+def writeClose(list, path):
+    for i in list:
+        try:
+            with open(path, 'w') as file:
+                file.write(i)
+            print("File written successfully.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    file.close()
+
+
