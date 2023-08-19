90d615fcb73d657ad9895eff822710f17fe6943e
RedDeliciouusApples <red-delicious-apple-hater@protonmail.com>
2023-08-19 13:19:26 -0700

    add list init

--- filewriter.py
+++ filewriter.py
@@ -5,4 +5,14 @@ def write(file_path, content):
         print("File written successfully.")
     except Exception as e:
         print(f"An error occurred: {e}")
-
+def init() -> list:
+    #starts a list with open html template
+    htmlList = []
+    htmlList.insert("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Blank HTML Template</title>
+        </head>
+        <body>""")
+    return htmlList
