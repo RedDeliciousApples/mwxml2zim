7b2084ddd25270e8584d7971bd91490e4cdc61bb
christian <christiansaliba@posteo.net>
2025-10-29 18:40:12 -0700

    rewrite of htmlhandler

--- htmlHandler.py
+++ htmlHandler.py
@@ -1,69 +1,62 @@
+from typing import Optional
 from wikitextprocessor import WikiNode, NodeKind
-
 import filewriter
 
-list = filewriter.init()
+_hlist: Optional[list] = None  # module-level shared state
 
+def begin():
+    #make sure to call this 1st
+    global _hlist
+    _hlist = filewriter.init()
+    return _hlist
 
-def level2(text):
-    content = "<h2> " + text + "</h2> <br />"
-    filewriter.write(content, list)
+def _require_init():
+    if _hlist is None:
+        raise RuntimeError("Call begin() before using writer functions. You did not call begin(), which is why you are getting this error.")
 
+def _write(content: str):
+    _require_init()
+    filewriter.write(content, _hlist)
 
-def level3(text):
-    content = "<h3> " + text + "</h3> <br />"
-    filewriter.write(content, list)
+def level2(text: str):
+    _write(f"<h2>{text}</h2><br />")
 
+def level3(text: str):
+    _write(f"<h3>{text}</h3><br />")
 
-def level4(text):
-    content = "<h4> " + text + "</h4> <br />"
-    filewriter.write(content, list)
+def level4(text: str):
+    _write(f"<h4>{text}</h4><br />")
 
+def level5(text: str):
+    _write(f"<h5>{text}</h5><br />")
 
-def level5(text):
-    content = "<h5> " + text + "</h5> <br />"
-    filewriter.write(content, list)
+def level6(text: str):
+    _write(f"<h6>{text}</h6><br />")
 
+def italic(text: str):
+    _write(f"<i>{text}</i>")
 
-def level6(text):
-    content = "<h6> " + text + "</h6> <br />"
-    filewriter.write(content, list)
-
-def italic(text):
-    content = "<i> " + text + "</i>"
-    filewriter.write(content, list)
-
-def bold(text):
-    content = "<b>" + text + "</b>"
-    filewriter.write(content, list)
+def bold(text: str):
+    _write(f"<b>{text}</b>")
 
 def hline():
-    content = "<hr>"
-    filewriter.write(content, list)
+    _write("<hr>")
 
 def handlelist(listNode: WikiNode):
-    level = len(listNode.sarg)
-    print("\n\n\nLIST FOUND!!!!\n\n\n")
-    #unordered list
-    if (listNode.sarg.__contains__("*")):
-        filewriter.write("<ul> ", list)
+    _require_init()
+    # unordered list
+    if "*" in listNode.sarg:
+        _write("<ul>")
         for item in listNode.children:
-            #handle multilevel lists
+            if item.kind == NodeKind.LIST_ITEM:
+                handlelist(item)  # if nested
+            _write(f"<li>{item.sarg}</li>")
+        _write("</ul>")
+    # ordered list
+    elif "#" in listNode.sarg:
+        _write("<ol>")
+        for item in listNode.children:
             if item.kind == NodeKind.LIST_ITEM:
                 handlelist(item)
-            content = "<li> " + item.sarg + "</li>"
-            filewriter.write(content, list)
-        print("\nLIST:\n" + filewriter.write("</ul>", list))
-
-    #ordered list
-    elif (listNode.sarg.__contains__("#")):
-        filewriter.write("<ol> ", list)
-        for item in listNode.children:
-            # handle multilevel lists
-            if item.kind == NodeKind.LIST_ITEM:
-                handlelist(item)
-            content = "<li> " + item.sarg + "</li>"
-            filewriter.write(content, list)
-        print("\n ORDERED LIST:\n" + filewriter.write("</ol>", list))
-
-    print("placeholder")
+            _write(f"<li>{item.sarg}</li>")
+        _write("</ol>")
